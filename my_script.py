8fbc1e941f8c59271476ed13d499cefa39e04d52
Mike Rossetti <s2t2@users.noreply.github.com>
2019-04-17 18:30:32 -0400

    Observing unexpected table structure

--- my_script.py
+++ my_script.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 print("GETTING HTML CONTENTS...")
 
 # TODO: use the requests package to get the contents of the page, instead of reading from file
-# ... if you need to get behind the paywall to access the page, you can use the ________ package to automate the login process
+# ... if you need to get behind the paywall to access the page, you can use the selenium package to automate the login process
 
 html_filepath = "kenpom-page.html"
 
@@ -15,7 +15,7 @@ soup = BeautifulSoup(open(html_filepath), features="lxml") # added the features 
 ratings_table = soup.find("table", id="ratings-table")
 print("RATINGS TABLE", type(ratings_table))
 
-rows = ratings_table.find("tbody").findAll("tr")
+rows = ratings_table.find("tbody").findAll("tr") #TODO: account for multiple tbody elements
 print("ROWS", type(rows), len(rows))
 
 for row in rows:
@@ -26,3 +26,13 @@ for row in rows:
     #team_name = cells[1].text #> includes the rank as well, so if you just want the team name...
     team_name = cells[1].find("a").text
     print(f"{rank}) {team_name}")
+
+    # hmm after number 40, seeing IndexError: list index out of range
+    # it appears the table contains multiple sets of thead + tbody. like:
+    # table
+    # ... thead
+    # ... tbody
+    # ... thead
+    # ... tbody
+    # ... thead
+    # ... tbody
